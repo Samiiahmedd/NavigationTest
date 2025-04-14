fbdf3f8aac1e0261b9aad33adb404d87c1df9626
Sami Ahmed <samiiiahmed8@gmail.com>
2025-04-14 02:24:24 +0200

    Initial Commit

---
+++ NavigationTest/NavigationTestApp.swift
@@ -0,0 +1,17 @@
+//
+//  NavigationTestApp.swift
+//  NavigationTest
+//
+//  Created by Sami Ahmed on 14/04/2025.
+//
+
+import SwiftUI
+
+@main
+struct NavigationTestApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
